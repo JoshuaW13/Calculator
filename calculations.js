f039c5870904b49c55b4f713107b4ccb109a620c
Josh <josh.walters@shaw.ca>
2022-08-27 19:00:53 -0600

    updated math logic

--- calculations.js
+++ calculations.js
@@ -14,14 +14,24 @@ function divide(num1, num2){
     return num1/num2;
 }
 
+function modulo(num1,num2){
+    return num1%num2;
+}
+
+function power(num1, num2){
+    return num1**num2;
+}
+
 function operate(e){
     let screen = document.querySelector('.calcScreen');
     if(nextClick === true){
         num2 = Number.parseFloat(screen.textContent);
     }
-    if(nextClick === false){
+    else if(nextClick === false){
         num1 = Number.parseFloat(screen.textContent);
     }
+
+    if(typeof num1 === 'undefined'){ans = screen.textContent;}
     
     screen.textContent = "";
     let ans;
@@ -29,8 +39,10 @@ function operate(e){
     else if(operator === "-"){ans = subtract(num1, num2);}
     else if(operator === "x"){ans = multiply(num1, num2);}
     else if(operator === "/"){ans = divide(num1, num2);}
+    else if(operator === "%"){ans = modulo(num1,num2);}
+    else if(operator === "Exp"){ans = power(num1,num2);}
     screen.textContent = ans;
-    console.log(num1+ " "+ num2+ nextClick);
+    console.log(num1+ " "+ num2+ +""+operator+nextClick+ans);
     nextClick = false;
     
 }
@@ -61,13 +73,41 @@ function setOperator(e){
         screen.textContent = "";
         nextClick = true;
     }
+    else if(this.textContent === "%"){
+        operator = "%";
+        if(screen.textContent != ""){num1 = Number.parseFloat(screen.textContent);}
+        screen.textContent = "";
+        nextClick = true;
+    }
+    else if(this.textContent === "Exp"){
+        operator = "Exp";
+        if(screen.textContent != ""){num1 = Number.parseFloat(screen.textContent);}
+        screen.textContent = "";
+        nextClick = true;
+    }
+    else if(this.textContent === "+/-"){
+        operator = "";
+        console.log(screen.textContent)
+        screen.textContent = screen.textContent;
+        if(screen.textContent.charAt(0) === "-"){
+            screen.textContent = screen.textContent.substring(1);
+        }
+        else{
+            screen.textContent = '-'+ screen.textContent;
+        }
+        
+    }
 
 
 }
 
 function printSymbol(e){
     let screen = document.querySelector('.calcScreen');
-    if(this.classList.contains("AC")){screen.textContent = "";}
+    if(this.classList.contains("AC")){
+        screen.textContent = "";
+        num1 = num2 = operator = "";
+
+    }
     else if(this.classList.contains('DEL')){screen.textContent = screen.textContent.slice(0,-1);}
     else if(this.classList.contains('operator')){return;}
     else{
